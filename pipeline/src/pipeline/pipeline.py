bf232b2e0b13e0001c8287f6adda0689dfd74861
jmargutti <jacopo.margutti@gmail.com>
2023-08-10 08:43:20 +0200

    updates

--- pipeline/src/pipeline/pipeline.py
+++ pipeline/src/pipeline/pipeline.py
@@ -18,28 +18,28 @@ logging.getLogger("").addHandler(console)
 load_dotenv(dotenv_path="../credentials/.env")
 
 
-def get_blob_service_client(blob_path, container):
+def get_blob_client(blob_path, container):
     blob_service_client = BlobServiceClient.from_connection_string(os.getenv("CONNECTION"))
     return blob_service_client.get_blob_client(container=container, blob=blob_path)
 
 
 def upload_file(container, file_path='messages.csv', blob_path='messages.csv'):
     # upload file to azure blob storage
-    blob_client = get_blob_service_client(blob_path, container)
+    blob_client = get_blob_client(blob_path, container)
     with open(file_path, "rb") as data:
         blob_client.upload_blob(data, overwrite=True)
 
 
 def download_file(container, file_path='messages.csv', blob_path='messages.csv'):
     # download file from azure blob storage
-    blob_client = get_blob_service_client(blob_path, container)
+    blob_client = get_blob_client(blob_path, container)
     with open(file_path, "wb") as download_file:
         download_file.write(blob_client.download_blob().readall())
 
 
-def list_files(container, blob_service_client: BlobServiceClient, container_name):
+def list_files(container):
     # list files in azure blob storage container
-    container_client = blob_service_client.get_container_client(container)
+    container_client = BlobServiceClient.from_connection_string(os.getenv("CONNECTION")).get_container_client(container)
     blob_list = container_client.list_blobs()
     return [f"{blob.name}" for blob in blob_list]
 
@@ -50,11 +50,9 @@ def main():
         tzinfo=datetime.timezone.utc).isoformat()
     container = os.getenv('CONTAINER')
 
-    download_file(container, 'local_directory/local_file.csv', 'blob_directory/blob_file.csv')
-    # do stuff
-    # ...
-    #
-    upload_file(container, 'local_directory/local_file.csv', 'blob_directory/blob_file.csv')
+    print(list_files(container))
+    download_file(container, 'test', 'ids/usage/jor-deh-staging-logs-extractor-s')
+    upload_file(container, 'test', 'ids/usage/jor-deh-staging-logs-extractor-s')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
 
